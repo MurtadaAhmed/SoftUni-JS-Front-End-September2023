4f55b4a5efeb1dce8107cf81cd1c721d1b96187a
Murtada Ahmed <murtadhabg@gmail.com>
2023-12-08 11:29:15 +0200

    added exercise

--- JS Front-End/DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Front-End/DOM Manipulation and Events/Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,41 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeTextArea = document.getElementsByTagName("textarea")[0];
+    let encodeButton = document.getElementsByTagName("button")[0];
+
+    let decodeTextArea = document.getElementsByTagName("textarea")[1];
+    let decodeButton = document.getElementsByTagName("button")[1];
+
+    encodeButton.addEventListener("click", encodeMessage);
+    decodeButton.addEventListener("click", decodeMessage);
+
+    function encodeMessage() {
+        let message = encodeTextArea.value;
+        let newMessage = "";
+
+        for (let i = 0; i < message.length; i++) {
+            charCodePlusOne = message.charCodeAt(i) + 1 // converting the character into charcode an adding one to it
+            newLetterAfterEncoding = String.fromCharCode(charCodePlusOne) // converting the new charcode back to string
+            newMessage += newLetterAfterEncoding
+            
+        }
+        decodeTextArea.value = newMessage;
+        encodeTextArea.value = "";
+    }
+
+    function decodeMessage() {
+
+        let message = decodeTextArea.value;
+        let newMessage = "";
+
+        for (let i = 0; i < message.length; i++) {
+            charCodeMinusOne = message.charCodeAt(i) - 1
+            newLetterAfterDecoding = String.fromCharCode(charCodeMinusOne)
+            newMessage += newLetterAfterDecoding
+            
+        }
+        decodeTextArea.value = "";
+        decodeTextArea.value = newMessage;
+        
+        
+    }
 }
