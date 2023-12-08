2c826ab08c643f02a41a769cad7ad99f34c6f3f4
Murtada Ahmed <murtadhabg@gmail.com>
2023-12-08 10:41:29 +0200

    added exercise

--- JS Front-End/DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Front-End/DOM Manipulation and Events/Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    text = document.querySelector("#newItemText").value;
+    value = document.querySelector("#newItemValue").value;
+    menu = document.querySelector("#menu")
+
+    option = document.createElement("option");
+    option.textContent = text;
+    option.value = value;
+
+    menu.appendChild(option);
+
+    document.querySelector("#newItemText").value = "";
+    document.querySelector("#newItemValue").value = "";
+
+
+
 }
