899f815910e0f7ab6e5c465ff4b24523b37b4ff8
Murtada Ahmed <murtadhabg@gmail.com>
2023-12-10 21:44:13 +0200

    added new exercises

--- JS Front-End/HTTP and AJAX/Exercise/03.Phonebook/app.js
+++ JS Front-End/HTTP and AJAX/Exercise/03.Phonebook/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    // TODO:
+    
+    document.getElementById('btnLoad').addEventListener('click', getAllPhones);
+    document.getElementById('btnCreate').addEventListener('click', createContact);
+    async function getAllPhones() {
+        const phoneInfo = await fetch('http://localhost:3030/jsonstore/phonebook');
+        const phoneInfoJson = await phoneInfo.json();
+
+        const phonebook = document.getElementById("phonebook");
+        phonebook.innerHTML = '';
+        Object.values(phoneInfoJson).forEach(contact => {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = 'Delete';
+            li.textContent = `${contact.person}: ${contact.phone}`;
+            li.appendChild(deleteButton);
+            phonebook.appendChild(li);
+            deleteButton.addEventListener('click', deleteContact);
+
+            function deleteContact() {
+                const id = contact._id;
+                fetch(`http://localhost:3030/jsonstore/phonebook/${id}`, {
+                    method: 'delete',
+                });
+                li.remove();
+
+            }
+
+        })
+    }
+
+    function createContact() {
+        const person = document.getElementById('person').value;
+        const phone = document.getElementById('phone').value;
+        const contact = {
+            person,
+            phone
+        }
+        fetch('http://localhost:3030/jsonstore/phonebook', {
+            method: 'post',
+            body: JSON.stringify(contact)
+        });
+        document.getElementById('person').value = '';
+        document.getElementById('phone').value = '';
+        getAllPhones()
+    }
+
+
 }
 
 attachEvents();
