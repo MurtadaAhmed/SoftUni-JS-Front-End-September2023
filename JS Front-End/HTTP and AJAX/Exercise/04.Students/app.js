80bd5ae31b488fc524adab315909329757051a57
Murtada Ahmed <murtadhabg@gmail.com>
2023-12-12 20:57:44 +0200

    uploaded exercise

--- JS Front-End/HTTP and AJAX/Exercise/04.Students/app.js
+++ JS Front-End/HTTP and AJAX/Exercise/04.Students/app.js
@@ -1,5 +1,64 @@
-function attachEvents() {
-  // TODO:
+async function attachEvents() {
+  const baseUrl = 'http://localhost:3030/jsonstore/collections/students';
+  const tableBody = document.querySelector('tbody');
+  const submitButton = document.getElementById('submit');
+
+  submitButton.addEventListener('click', addStudent);
+  
+  const getResponse = await fetch(baseUrl);
+
+  const students = await getResponse.json();
+
+  Object.values(students).forEach(student => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <tr>
+    <td>${student.firstName}</td>
+    <td>${student.lastName}</td>
+    <td>${student.facultyNumber}</td>
+    <td>${student.grade}</td>
+    </tr>`
+    tableBody.appendChild(row);
+  })
+
+  async function addStudent(){
+    const [firstName, lastName, facultyNumber, grade] = document.querySelectorAll('#form input');
+    if(!firstName.value || !lastName.value || !facultyNumber.value || !grade.value){
+      return;
+    }
+
+    const newStudentInfo = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      facultyNumber: facultyNumber.value,
+      grade: grade.value
+    }
+
+    await fetch(baseUrl, {
+      method: 'post',
+      body: JSON.stringify(newStudentInfo),
+      
+    })  
+    const row = document.createElement('tr');
+    row.innerHTML = `
+    <tr>
+    <td>${newStudentInfo.firstName}</td>
+    <td>${newStudentInfo.lastName}</td>
+    <td>${newStudentInfo.facultyNumber}</td>
+    <td>${newStudentInfo.grade}</td>
+    </tr>`
+    tableBody.appendChild(row);
+
+
+    firstName.value = '';
+    lastName.value = '';
+    facultyNumber.value = '';
+    grade.value = '';
+    
+    }
+
+    
+  
 }
 
 attachEvents();
--- JS Front-End/HTTP and AJAX/Exercise/05. Locked-Profile/app.js
+++ JS Front-End/HTTP and AJAX/Exercise/05. Locked-Profile/app.js
@@ -1,3 +1,57 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const baseUrl = 'http://localhost:3030/jsonstore/advanced/profiles';
+    const main = document.getElementById('main');
+ 
+    main.innerHTML = '';
+ 
+    fetch(baseUrl)
+        .then(res => res.json())
+        .then(data => {
+            Object.values(data).forEach((profile, i) => {
+                const div = document.createElement('div');
+                div.classList.add('profile');
+                console.log(profile);
+                debugger;
+ 
+                div.innerHTML = `
+                    <img src="./iconProfile2.png" class="userIcon" />
+                    <label>Lock</label>
+                    <input type="radio" name="user${i + 1}Locked" value="lock" checked>
+                    <label>Unlock</label>
+                    <input type="radio" name="user${i + 1}Locked" value="unlock"><br>
+                    <hr>
+                    <label>Username</label>
+                    <input type="text" name="user${i + 1}Username" value="${profile.username}" disabled readonly />
+                    <div class="user${i + 1}Username" style="display: none;">
+                        <hr>
+                        <label>Email:</label>
+                        <input type="email" name="user${i + 1}Email" value="${profile.email}" disabled readonly />
+                        <label>Age:</label>
+                    <input type="email" name="user${i + 1}Age" value="${profile.age}" disabled readonly />
+                    </div>
+                    <button>Show more</button>
+                `;
+ 
+                main.appendChild(div);
+ 
+                const button = div.querySelector('button');
+                button.addEventListener('click', onToggle);
+ 
+            });
+        });
+ 
+    function onToggle(ev) {
+        const profile = ev.target.parentElement;
+        const isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+ 
+        if (isActive) {
+            const info = profile.querySelector('div');
+            const isVisible = info.style.display == 'block';
+ 
+            info.style.display = isVisible ? 'none' : 'block';
+            ev.target.textContent = isVisible ? 'Show more' : 'Hide it';
+        }
+    }
 }
+ 
+window.onload = lockedProfile;
